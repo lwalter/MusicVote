26cf261a0502a15f8fea9a43e986a395ca50fb38
lwalter1022@hotmail.com <lwalter1022@hotmail.com>
2015-03-17 21:24:04 -0700

    Changed from $.ajax to $.post

--- MusicVoteApp/static/MusicVoteApp/musicChannelLib.js
+++ MusicVoteApp/static/MusicVoteApp/musicChannelLib.js
@@ -1,15 +1,10 @@
 <!--
     $(document).ready(function() {
         $(".voteBtn").click(function() {
-            $.ajax({
-                url: '/vote/',
-                data: {'musicchannel': document.getElementById("channel-title").getAttribute("data-channel-id"), 
-                        'song': $(this).val()},
-                type: 'POST',
-                failure: function(data) {
-                    alert("Error");
-                }
-            });
+            $.post('/vote/',
+                    {'musicchannel': document.getElementById("channel-title").getAttribute("data-channel-id"), 
+                        'song': $(this).val()}
+            );
         });
     });
 -->
